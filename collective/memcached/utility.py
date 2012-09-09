3cff44ac752fff10c1e2220772b1e84258094c8c
Manabu TERADA <terada@cmscom.jp>
2012-09-09 18:55:45 +0900

    Create new MemcacheAdapter

--- collective/memcached/utility.py
+++ collective/memcached/utility.py
@@ -6,8 +6,12 @@ except ImportError:
 import logging
 
 from threading import local
-from zope.component import getUtility
+from zope.component import getUtility, queryUtility
+from zope.interface import implements
+from zope.ramcache.interfaces.ram import IRAMCache
 from plone.registry.interfaces import IRegistry
+from plone.memoize.interfaces import ICacheChooser
+from plone.memoize import ram
 from collective.memcached.interfaces import IMemcachedControlPanel
 
 
@@ -38,3 +42,38 @@ class MemcachedClient(object):
     def getSettings(self):
         registry = getUtility(IRegistry)
         return registry.forInterface(IMemcachedControlPanel)
+
+class MemcachedCacheChooser(object):
+    """"""
+    implements(ICacheChooser)
+    _v_thread_local = local()
+
+    def __call__(self, fun_name):
+        """
+        Create new adapter for plone.memoize.ram.
+        """
+        client = self.getClient()
+        if client is not None:
+            return ram.MemcacheAdapter(client=client, globalkey=fun_name)
+        else:
+            return ram.RAMCacheAdapter(queryUtility(IRAMCache),
+                                            globalkey=fun_name)
+
+    def getClient(self):
+        """Return thread local connection to memcached.
+        """
+        connection = getattr(self._v_thread_local, 'connection', None)
+
+        if connection is None:
+            registry = getUtility(IRegistry)
+            try:
+                settings = registry.forInterface(IMemcachedControlPanel)
+                connection = memcache.Client(settings.memcached_hosts)
+            except KeyError:
+                logger.info("Can't create memcache connection")
+            finally:
+                logger.info("Creating new memcache connection")
+                self._v_thread_local.connection = connection
+
+        return connection
+
